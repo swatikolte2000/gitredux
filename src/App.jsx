9da75766a47db76f4dba382344287a481863c9ae
swatikolte2000 <swatikolte2000@gmail.com>
2024-12-02 21:37:49 +0530

    initialise

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -17,10 +18,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Add here</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+       
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -28,6 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    
     </>
   )
 }
